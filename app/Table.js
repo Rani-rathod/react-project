d01602a4ada8f85e121f98b266f6171a990ab0e9
Rani Rathod <ranirathor20@navgurukul.org>
2024-10-15 18:39:39 +0530

    fixed some things

--- app/Table.js
+++ app/Table.js
@@ -3,20 +3,34 @@ import React, { useState, useEffect } from 'react';
 
 const Table = () => {
   const [todos, setTodos] = useState([]);
+  const [searchId, setSearchId] = useState('');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setTodos(data);
       })
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
+  const filteredTodos = searchId
+    ? todos.filter((todo) => todo.id === parseInt(searchId))
+    : todos;
+
   return (
     <div>
       <h1 className='list'>Todos List</h1>
+      
+      <div className='inputid'>
+        <input
+          type="text"
+          placeholder="Enter ID..."
+          value={searchId}
+          onChange={(e) => setSearchId(e.target.value)}
+        />
+      </div>
+      
       <table border="1" cellPadding="10" cellSpacing="0">
         <thead>
           <tr>
@@ -27,17 +41,24 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          {todos.map((todo) => (
-            <tr key={todo.id}>
-              <td>{todo.userId}</td>
-              <td>{todo.id}</td>
-              <td>{todo.title}</td>
-              <td>{todo.completed ? 'True' : 'False'}</td>
+          {filteredTodos.length > 0 ? (
+            filteredTodos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.userId}</td>
+                <td>{todo.id}</td>
+                <td>{todo.title}</td>
+                <td>{todo.completed ? 'True' : 'False'}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>No data found</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
   );
 };
+
 export default Table;
